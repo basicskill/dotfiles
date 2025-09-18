2ecc7f771eaaca1f6e2f69fa3b7075bc651fe476
Mladen <mladen@signavatar.org>
2025-09-18 12:51:12 +0200

    Started setup script

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Predefined list of file names
+files=("zshrc" "shell_aliases")
+
+# Loop through each file name
+for file in "${files[@]}"; do
+    # Check if source file exists
+    if [ ! -f "$file" ]; then
+        echo "Warning: Source file '$file' does not exist, skipping..."
+        continue
+    fi
+    
+    # Check if destination already exists
+    if [ -e "$HOME/.$file" ]; then
+	    rm "$HOME/.$file"
+    fi
+    
+    # Create symbolic link
+    ln -s "$(pwd)/$file" "$HOME/.$file"
+    
+    # Verify the link was created successfully
+    if [ $? -eq 0 ]; then
+	echo "✓ Created symlink: ~/.$file -> $(pwd)/$file"
+    else
+        echo "✗ Failed to create symlink for $file"
+    fi
+done
+
+echo "Symbolic link creation completed!"
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+## Install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
--- zshrc
+++ zshrc
@@ -14,18 +14,5 @@ plugins=(
 
 source $ZSH/oh-my-zsh.sh
 
-
-# User configuration
-export MANPATH="/usr/local/man:$MANPATH"
-autoload -Uz compinit
-compinit
-
 # Load aliases
 [ -f ~/.shell_aliases ] && . ~/.shell_aliases
-
-# Functions 
-cheat() { curl cheat.sh/$1 }
-hotspot() { sudo create_ap wlp3s0 enp0s25 "Mladenov ThinkPad" raskoljnikov }
-
-# If exists, load working directory location
-[ -f ~/.working ] && . ~/.working
